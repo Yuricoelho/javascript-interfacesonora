8c2043acf95c99ed00fd9b36ca1811675f4cd39e
Yuri <yuricoelho33@gmail.com>
2021-12-03 19:25:42 -0300

    Adicionando o recurso de ativar os botões quando o usuário utilizar o teclado.

--- main.js
+++ main.js
@@ -13,6 +13,10 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
+
+    tecla.onkeydown = function () {
+        tecla.classList.add('ativa');
+    }
 }
 
 
